d93d304c1dd23e1ae1aa829b2307458769eb8002
Manish Makwana <manish.makwana@ergo.co.nz>
2017-03-15 15:23:52 +1300

    updates to config.

--- backup.sh
+++ backup.sh
@@ -1,5 +1,5 @@
 rsync -a --delete /var/www/dokuwiki/ /vagrant/dokuwiki-data-mirror
-git add -A
-timestmp=$(perl -e 'BEGIN {use POSIX;} print strftime("%Y-%m-%d %H:%M:%S ", localtime)')
-commit_message="backup on $timestmp"
-git commit -m $commit_message
+#git add -A
+#timestmp=$(perl -e 'BEGIN {use POSIX;} print strftime("%Y-%m-%d %H:%M:%S ", localtime)')
+#commit_message="backup on $timestmp"
+#git commit -m $commit_message
